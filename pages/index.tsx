e684eb867a280a35306aabede582da4cb94f254a
Jorge Arreola <jorgitoarreolas@gmail.com>
2021-06-17 17:42:48 -0500

    testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,24 +3,24 @@ import { Button } from 'react-bootstrap'
 import Layout from '../components/layout'
 
 const Home: React.FC = () => {
-    const title = "hola"
-    
-    const handleLog =(text: string): void => {
-        console.log(text)
-    }
+  const title = 'aleluya'
+
+  const handleLog = (text: string): void => {
+    console.log(text)
+  }
 
   return (
-          <Layout>
-              <p>{title}</p>
+    <Layout>
+      <p>{title}</p>
       <h1>Hola</h1>
-      <Button variant="primary" onClick={()=>handleLog("Hola")}>Hola</Button>
+      <Button variant="primary" onClick={() => handleLog('Hola')}>
+        Hola
+      </Button>
       <Button variant="success">Hola</Button>
       <Button variant="danger">Hola</Button>
       <Button variant="whatsapp">Hola</Button>
-        <Button className="whatsapp"></Button>
-        <Button className="whatsapp">
-            Osiosi    
-        </Button>
+      <Button className="whatsapp"></Button>
+      <Button className="whatsapp">Osiosi</Button>
     </Layout>
   )
 }

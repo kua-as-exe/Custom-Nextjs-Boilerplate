93e883bbfc4ea09adf56b29c99c413f4283f03b4
Jorge Arreola <jorgitoarreolas@gmail.com>
2021-06-17 11:33:39 -0500

    eslint & prettify

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,27 @@
 import { Button } from 'react-bootstrap'
 import Layout from '../components/layout'
 
-export default function Home() {
+const Home: React.FC = () => {
+    const title = "hola"
+    
+    const handleLog =(text: string): void => {
+        console.log(text)
+    }
+
   return (
-    <Layout>
+          <Layout>
+              <p>{title}</p>
       <h1>Hola</h1>
-      <Button variant="primary">Hola</Button>
+      <Button variant="primary" onClick={()=>handleLog("Hola")}>Hola</Button>
       <Button variant="success">Hola</Button>
       <Button variant="danger">Hola</Button>
       <Button variant="whatsapp">Hola</Button>
+        <Button className="whatsapp"></Button>
+        <Button className="whatsapp">
+            Osiosi    
+        </Button>
     </Layout>
   )
 }
+
+export default Home
